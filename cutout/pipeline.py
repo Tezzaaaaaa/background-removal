4d665c4f8ea8796c7c4cfc17ee1fd8e326e22200
Tereroa <kuresa_afamasaga@yahoo.com>
2026-09-04 10:24:27 +1000

    Use U2Net as automatic segmentation fallback

--- cutout/pipeline.py
+++ cutout/pipeline.py
@@ -8,9 +8,20 @@ from .segmenter import CutoutSegmenter
 
 class CutoutPipeline:
     def __init__(self):
-        self.segmenter = CutoutSegmenter()
+        try:
+            self.segmenter = CutoutSegmenter()
+            self.engine = "birefnet"
+        except Exception as primary_error:
+            from .u2net import U2NetSegmenter
+
+            try:
+                self.segmenter = U2NetSegmenter()
+                self.engine = "u2net"
+            except Exception:
+                raise primary_error
 
     def process(self, image: Image.Image) -> Image.Image:
         alpha = self.segmenter.predict_alpha(image)
-        alpha = refine_alpha(self.segmenter, image, alpha)
+        if self.engine == "birefnet":
+            alpha = refine_alpha(self.segmenter, image, alpha)
         return decontaminate(image, alpha)
