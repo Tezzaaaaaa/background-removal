42ac051900ae92d4a189bcdfb93b6763727b82fe
Tereroa <kuresa_afamasaga@yahoo.com>
2026-09-08 05:47:49 +1000

    Add JavaScript Cutout public API

---
+++ cutout/index.js
@@ -0,0 +1,5 @@
+export { CutoutPipeline } from './pipeline.js';
+export { Segmenter, CutoutSegmenter, INPUT_SIZE } from './segmenter.js';
+export { U2NetSegmenter, MODEL_URL, MODEL_MD5, MODEL_PATH, ensureModel } from './u2net.js';
+export { refineAlpha } from './refiner.js';
+export { decontaminate } from './decontaminate.js';
